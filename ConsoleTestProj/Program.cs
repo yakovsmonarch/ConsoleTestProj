1601ea033b814a4baeaa83b280005cba3278d512
Яков Серебренников <serebrennikov@rbsoft.ru>
2023-05-28 19:39:32 +0800

    Добавьте файлы проекта.

---
+++ ConsoleTestProj/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".net3.1 Hello Ubuntu 20.04!!!");
+            Console.ReadLine();
+        }
+    }
+}
